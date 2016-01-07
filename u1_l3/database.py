2f0bc10b24c9b1ce4b794bc3ecfa5b451ed5ea25
Matthew <m.zadrozny@gmail.com>
2016-01-07 16:08:50 -0500

    Trying to fix indents on weather data

--- u1_l3/database.py
+++ u1_l3/database.py
@@ -40,16 +40,16 @@ cities = (
 	)
 
 weather = (
-    ('New York City', 2013, 'July', 'January', 62),
-    ('Boston', 2013, 'July', 'January', 59),
-    ('Chicago', 2013, 'July', 'January', 59),
-    ('Miami', 2013, 'August', 'January', 84),
-    ('Dallas', 2013, 'July', 'January', 77),
-    ('Seattle', 2013, 'July', 'January', 61),
-    ('Portland', 2013, 'July', 'December', 63),
-    ('San Francisco', 2013, 'September', 'December', 64),
-    ('Los Angeles', 2013, 'September', 'December', 75)
-    )	
+	('New York City', 2013, 'July', 'January', 62),
+	('Boston', 2013, 'July', 'January', 59),
+	('Chicago', 2013, 'July', 'January', 59),
+	('Miami', 2013, 'August', 'January', 84),
+	('Dallas', 2013, 'July', 'January', 77),
+	('Seattle', 2013, 'July', 'January', 61),
+	('Portland', 2013, 'July', 'December', 63),
+	('San Francisco', 2013, 'September', 'December', 64),
+	('Los Angeles', 2013, 'September', 'December', 75)
+	)	
 
 
 con = lite.connect('getting_started.db')
